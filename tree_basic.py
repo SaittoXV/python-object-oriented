1266bd0ae75c00cbdf24d4ff2de6c821b460a272
Abu Bakar Syed <syed.abubakar73@gmail.com>
2026-04-30 17:42:00 +0800

    add top bottom function

--- tree_basic.py
+++ tree_basic.py
@@ -7,11 +7,18 @@ class Node:
 def inorder(node):
     if node:
         inorder(node.left)
-        print(node)
-        inorder(nide.right)
+        print(node.value)
+        inorder(node.right)
+
+def topbottom(node):
+    if node:
+        print(node.value)
+        topbottom(node.left)
+        topbottom(node.right)
 
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 inorder(root)
+topbottom(root)
 
